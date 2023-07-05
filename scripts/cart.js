a721eaf2a720007bc554e35b78738a631ed5d018
AnandAK07 <anandkumark055@gmail.com>
2023-07-06 03:35:18 +0530

    static

---
+++ scripts/cart.js
@@ -0,0 +1,299 @@
+// var  cartData=[
+
+//   {  brand_name:"Louis Philip",
+// productType:"mansclothing",
+//     name:"LP Brand - Symbol Men Shirt",
+//    image_url:"https://m.media-amazon.com/images/I/A1wEjvjUA-L._AC_UL480_FMwebp_QL65_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/A1yExRGrofL._AC_UX679_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/91ELg13MgUL._AC_UX679_.jpg",
+//    strikedoffprice: "799",
+//    price: "557",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"Louis Philip",
+// productType:"mansclothing",
+//     name:"LP Brand - Symbol Men Shirt",
+//    image_url:"https://m.media-amazon.com/images/I/911xrpb4BGL._AC_UL480_FMwebp_QL65_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/91feHr7BH1L._AC_UY879_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/91EiDMKUUuL._AC_UY879_.jpg",
+//    strikedoffprice: "799",
+//    price: "557",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+
+// {  brand_name:"Louis Philip",
+// productType:"mansclothing",
+//     name:"LP Brand - Symbol Men Shirt",
+//    image_url:"https://m.media-amazon.com/images/I/71ORz8flzkL._AC_UL480_QL65_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/61pO6WKIePL._AC_UY879_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/71Md5hfnoLL._AC_UY879_.jpg",
+//    strikedoffprice: "799",
+//    price: "557",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+
+// {  brand_name:"Louis Philip",
+// productType:"mansclothing",
+//     name:"LP Brand - Symbol Men Shirt",
+//    image_url:"https://m.media-amazon.com/images/I/61UqWXWlsbL._AC_UL480_FMwebp_QL65_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/81NP3Awt2VL._AC_UY879_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/71TsJcKT6BL._AC_UY879_.jpg",
+//    strikedoffprice: "799",
+//    price: "557",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+
+//    {  brand_name:"NIVIA",
+//    productType:"mansCream",
+//     name:"Nivea Men Creme, Dark Spot Reduction, Non Greasy Moisturizer, Cream With Uv Protect, 150 ml",
+//    image_url:"https://m.media-amazon.com/images/I/51NYSJRXJ6L._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/51J0i8pWNmL._SY450_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/61WcUrkdpYL._SY450_.jpg",
+//    strikedoffprice: "399",
+//    price: "259",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"MAN MATTERS",
+//   productType:"mansCream",
+//     name:"Man Matters Biotin Hair Gummies | 60 Days Pack | With Biotin, |100% Vegetarian | 60 Gummies",
+//    image_url:"https://m.media-amazon.com/images/I/81bt4NnbTOL._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/81jL0EGMmfL._SX569_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/812po+vEY3L._SX569_.jpg",
+//    strikedoffprice: "849",
+//    price: "799",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"The Man Company",
+// productType:"mansCream",
+//     name:"The Man Company Skin Brightening Cream with Multani Mitti, Coco Butter | Removes Blackhead - 30 gm",
+//    image_url:"https://m.media-amazon.com/images/I/51QZU5qQRVL._SY450_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/71YpI+Nin1L._SY450_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/71LBwLbxtxL._SY450_.jpg",
+//    strikedoffprice: "299",
+//    price: "189",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"GARNIER",
+// productType:"mansCream",
+//     name:"Garnier Men, Face Wash, Brightening & Anti-Pollution, TurboBright Double Action, 150 g",
+//    image_url:"https://m.media-amazon.com/images/I/51YOFiC3zvL._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/61pOAOfbDML._SX425_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/61qEEmhOTLL._SX425_.jpg",
+//    strikedoffprice: "325",
+//    price: "262",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"URBAN FOREST",
+// productType:"wallet",
+//     name:"URBAN FOREST Oliver Redwood Leather Wallet for Men",
+//    image_url:"https://m.media-amazon.com/images/I/71t7CgBwJ4L._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/81wc7fARCsL._SY741_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/81T8LdEzLQL._SX569_.jpg",
+//    strikedoffprice: "2000",
+//    price: "499",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"RIGOHILL",
+// productType:"wallet",
+//     name:"RIGOHILL Doger Olive Green Mens Leather Wallet | Leather Wallet for Men | RFID Mens Wallet",
+//    image_url:"https://m.media-amazon.com/images/I/71SYvtX2YmL._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/71lhhYRTFpL._SX569_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/81FlhNtY3NL._SX569_.jpg",
+//    strikedoffprice: "2499",
+//    price: "499",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"NAPA HIDE",
+// productType:"wallet",
+//     name:"NAPA HIDE Leather Wallet for Men I Handcrafted I Multiple Credit | I 2 Secret Compartments",
+//    image_url:"https://m.media-amazon.com/images/I/81aZAmkJQWL._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/91GgbctZ1EL._UX679_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/81gZOqQXT9L._UX679_.jpg",
+//    strikedoffprice: "1499",
+//    price: "299",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"WildHorn",
+// productType:"wallet",
+//     name:"WildHorn Trifold Leather Wallet for Men I Handcrafted I Ultra Strong Stitching | I Currency Compartment",
+//    image_url:"https://m.media-amazon.com/images/I/71SJJMXG19L._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/813-bVZMpIL._SX569_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/81LLM2H7xsL._SX569_.jpg",
+//    strikedoffprice: "1499",
+//    price: "299",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"Fastrac",
+// productType:"mansSunglasses",
+//     name:"Fastrack Men Square Black Sunglasses-Pack of 1",
+//    image_url:"https://m.media-amazon.com/images/I/61+THk53YIL._AC_UL400_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/619hy5yVQWL._UX522_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/61vPBNLue7L._UX679_.jpg",
+//    strikedoffprice: "799",
+//    price: "699",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"VELOCITY STORE",
+// productType:"mansSunglasses",
+//     name:"Velocity Polarized Aviator Green INJ POL Sunglasses for Men",
+//    image_url:"https://m.media-amazon.com/images/I/51TZG3rY+DL._UX679_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/51J7FIerYCL._UX679_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/518KAsYiirL._UX679_.jpg",
+//    strikedoffprice: "1499",
+//    price: "1299",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"Fastrack",
+// productType:"mansSunglasses",
+//     name:"Fastrack Men Pilot Green Sunglasses-Pack of 1",
+//    image_url:"https://m.media-amazon.com/images/I/61XFwxOh4GL._UX679_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/61X3yRvhrLL._UX679_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/71RLeDOfVEL._UX522_.jpg",
+//    strikedoffprice: "2099",
+//    price: "1965",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"ELEGANTE",
+// productType:"mansSunglasses",
+//     name:"ELEGANTE Mens Square Sunglasses, UV Protected Stylish Sun Glasses for Men",
+//    image_url:"https://m.media-amazon.com/images/I/51bzps707YL._UX679_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/71lCmJtpyHL._UX679_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/61vE4kusVTL._UX679_.jpg",
+//    strikedoffprice: "2499",
+//    price: "596",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"FREECULTR",
+// productType:"mansclothing",
+//     name:"FREECULTR Unisex Printed Bamboo Bandana Face Mask for Dust and Sun Protection",
+//    image_url:"https://m.media-amazon.com/images/I/718-a6R5nWL._UX425_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/71CS9pbsQPL._UX425_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/71xrPzGy+0L._UX425_.jpg",
+//    strikedoffprice: "999",
+//    price: "499",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"Lymio",
+// productType:"mansclothing",
+//     name:"Lymio Casual Shirt for Men|| Shirt for Men|| Men Stylish Shirt || Men Printed Shirt (Patta)",
+//    image_url:"https://m.media-amazon.com/images/I/71On2XSPuRL._UY445_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/81300Ou0hAL._UY445_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/7143xg-yZVL._UY445_.jpg",
+//    strikedoffprice: "4999",
+//    price: "379",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"Amazon",
+// productType:"mansclothing",
+//     name:"Amazon Brand - Symbol Men Shirt",
+//    image_url:"https://m.media-amazon.com/images/I/81gEEU2IeHL._UX522_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/81jMs8fB4fL._UX522_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/91yl4ysaxoL._UX522_.jpg",
+//    strikedoffprice: "2099",
+//    price: "557",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// },
+// {  brand_name:"Amazon",
+// productType:"mansclothing",
+//     name:"Amazon Brand - Symbol Men Formal Shirt",
+//    image_url:"https://m.media-amazon.com/images/I/713xbqIKW4L._UX522_.jpg",
+//    image_url1:"https://m.media-amazon.com/images/I/81tuLcV5zTL._UX522_.jpg",
+//    image_url2:"https://m.media-amazon.com/images/I/61PNLI3SYrL._UX522_.jpg",
+//    strikedoffprice: "1699",
+//    price: "836",
+//    save: "save 30%",
+//      extrasave : "Extra 15% OFF"
+// }
+//  ];
+// // let cartData=JSON.parse(localStorage.getItem("cartPage"))||[];
+
+// function displayCart(data){
+    
+//         let parent=document.getElementById("cartPage");
+//         parent.innerHTML="";
+//         let bag=document.createElement("h2");
+//         bag.textContent=`Your bag(${data.length} items)`
+//         let notification=document.createElement("h3");
+//         notification.textContent="Congrats! You get free standard shopping"
+//         let line=document.createElement("hr");
+//         parent.append(bag,notification,line);
+       
+       
+//         data.map((el)=>{
+//         let cart=document.createElement("div");
+//         let left=document.createElement("div");
+        
+//         let right=document.createElement("div");
+//         let image=document.createElement("img");
+//         image.src=el.image_url;
+        
+//         let name=document.createElement("a");
+//         name.textContent=el.name;
+//         let price=document.createElement("p");
+//         price.textContent=el.price;
+//         let top=document.createElement("div");
+        
+//         let likebtn=document.createElement("button");
+//         top.append(name,likebtn);
+//         let buttom=document.createElement("div");
+//         // let increase=document.createElement("button");
+//         // let decrease=document.createElement("button");
+//         let remove=document.createElement("button");
+//         remove.textContent="Remove";
+//         buttom.append(price);
+//         let line=document.createElement("hr");
+//         left.append(image);
+//         right.append(top,buttom);
+//         cart.append(remove,left,right);
+//         parent.append(cart,line);
+//     })
+// }
+
+// displayCart(cartData);
+
+
+const openBtn=document.getElementById('open_cart_btn')
+const cart=document.getElementById('side_cart')
+const closeBtn=document.getElementById('close_btn')
+const backdrop=document.querySelector('.backdrop')
+
+openBtn.addEventListener('click',openCart)
+closeBtn.addEventListener('click',closeCart)
+
+//Open Cart
+function openCart(){
+  cart.classList.add('open')
+  backdrop.style.display='block'
+  setTimeout(()=>{
+    backdrop.classList.add('show');
+  },0)
+}
+
+//Close Cart
+function closeCart(){
+  cart.classList.remove('open')
+  backdrop.classList.remove('show');
+  
+  setTimeout(()=>{
+    backdrop.style.display='none'
+  },500)
+}

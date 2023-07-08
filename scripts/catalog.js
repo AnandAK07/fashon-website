5da63926e216f2be141f3cb51025aa16518e85be
abhijasud <abhijasud9@gmail.com>
2023-07-08 16:40:48 +0530

    sheeeeeeeeesh

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -8,7 +8,7 @@ const data = [
 		long_desc_title: "An instant, dewy glow that lasts.",
 		long_desc: "An oil-serum hybrid that's a one-step shortcut to the way your skin looks after a full skincare routine. Packed with some of skincare’s most hydrating heavy-hitters, nourishing oils and powerful plant-based extracts give you a gleamy, well-moisturized, brighter look that lasts up to 12 hours.",
 		second_img: "https://glossier-prod.imgix.net/products/futuredew-9.jpg?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist tested, Good for all skin types",
 	},
 	{
@@ -20,7 +20,7 @@ const data = [
 		long_desc_title: "Our cult-favorite, do-everything lip balm.",
 		long_desc: "Our universally-loved lip balm has been a fan favorite since 2014. Formulated with antioxidants and natural emollients, the vegan, does-it-all formula immediately nourishes dry, chapped lips and keeps them quenched for hours. The specially-designed applicator makes sure your balm applies exactly where you want it. With this one, you can do it all and bring it everywhere.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-bdc-wildfig-carousel-09.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist-tested, long-lasting moisture",
 	},
 	{
@@ -32,7 +32,7 @@ const data = [
 		long_desc_title: "A super gentle cleansing experience.",
 		long_desc: "A nourishing and pH-balanced gel-cream cleanser that plays nicely with all skin types. Conditioning ingredients leave skin soft, smooth and never stripped to start your day. Then dissolves dirt, oil and makeup when you’re ready to wipe the day away.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-mjc-carousel-08.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist-tested, formulated without fragrance",
 	},
 	{
@@ -44,7 +44,7 @@ const data = [
 		long_desc_title: "A plant-powered deep clean",
 		long_desc: "A highly concentrated, purifying cleanser that gently exfoliates, clarifies and re-energizes skin in 60 seconds. An A-Team of powerful plant exfoliators—Grape Extract and Lactic Acid—gently smooth skin and brighten your glow, while amino acid surfactants create an undetectable film that holds on to key ingredients so they can work on the skin instead of just rinsing down the drain.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-cleanser-concentrate-carousel-08.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist tested, formulated without fragrance, recyclable bottle",
 	},
 	{
@@ -56,7 +56,7 @@ const data = [
 		long_desc_title: "Light and buildable moisture for a dewy, smooth canvas.",
 		long_desc: "A lightweight, buildable moisturizer that instantly brings out the best in all skin types. It hydrates, visibly evens skin texture, and minimizes the look of pores—while priming skin for gorgeous makeup application.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-priming-moisturizer-carousel-06.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist-tested, long-lasting moisture, formulated without fragrance, good for for all skin types",
 	},
 	{
@@ -68,7 +68,7 @@ const data = [
 		long_desc_title: "One step to moisturize, control shine, and refine the look of pores—specifically formulated for oily and combination skin.",
 		long_desc: "A fast-absorbing, lightweight gel-cream moisturizer that balances oil without over-drying or leaving a powdery finish. Immediately, thanks to Marine Extract and Niacinamide, pores appear smaller, shine is gone, and skin is hydrated and happy—with more skin-balancing benefits over time.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-priming-moisturizer-balance-carousel-08.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist-tested, formulated without fragrance, alcohol and oil-free, gluten and dairy-free, GMO-free",
 	},
 	{
@@ -80,7 +80,7 @@ const data = [
 		long_desc_title: "Dry skin’s best friend—an ultra rich, buttery moisturizer.",
 		long_desc: "A luxuriously rich yet fast-absorbing face cream that delivers a surge of long-lasting hydration. The buttery texture and blend of botanical oils and glycerin melt into skin to give you a smooth, dewy finish that’s perfectly prepped for makeup.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-priming-moisturizer-rich-carousel-08.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Formulated without fragrance, silicone-free, recyclable glass",
 	},
 	{
@@ -92,7 +92,7 @@ const data = [
 		long_desc_title: "Sunscreen for people who hate wearing sunscreen.",
 		long_desc: "Lightweight and undetectable, this water-gel SPF 35 applies like a serum, with no greasy residue or white cast. Specially designed so even the sunscreen haters can't help but fall in love with it, Invisible Shield helps prevent sun damage and sun burn, while neutralizing harmful free radicals. Plus, the active microcapsules instantly absorb onto skin and play well with makeup, so you really have no reason to skip SPF.",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-invisible-shield-carousel-07.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist-tested, plays well with all skin types",
 	},
 	{
@@ -104,25 +104,25 @@ const data = [
 		long_desc_title: "A puffer jacket for your skin.",
 		long_desc: "A daily moisture barrier recovery cream perfect for very dry and/or sensitive skin. The buttery, cocooning cream formulated with plant-based moisture magnets, Glycerin and Cupuaçu Butter, helps dry skin bounce back and lock in hydration for up to 24 hours. Awarded the National Eczema Association Seal of Acceptance",
 		second_img: "https://glossier-prod.imgix.net/products/glossier-after-baume-carousel-08.png?auto=compress,format&cs=srgb&w=540",
-		uses: "", 
+		uses: "",
 		uses_test: "Dermatologist tested, formulated without fragrance",
 	},
 ];
 
-async function renderPage(){
-	let response  = await fetch("http://localhost:3000/data");
+async function renderPage() {
+	let response = await fetch("http://localhost:3000/data");
 	let data = await response.json();
 	const content = document.querySelector(".content");
 	data.forEach((elem) => {
 		const cardDiv = document.createElement("div");
 		cardDiv.classList.add("card");
-	
+
 		const image_a = document.createElement("a");
 		const primary_image = document.createElement("img");
 		primary_image.src = `${elem.primary_image}`;
 		image_a.href = `product_details.html/?id=${elem.id}`
 		image_a.appendChild(primary_image);
-	
+
 		const title_price = document.createElement("div");
 		title_price.classList.add("title-price");
 		const title = document.createElement("h3");
@@ -132,25 +132,25 @@ async function renderPage(){
 		price.textContent = `$${elem.price}`
 		price.classList.add("price");
 		title_price.append(title, price);
-	
+
 		const desp = document.createElement("p");
 		desp.classList.add("desp");
 		desp.textContent = elem.desc;
-	
+
 		const button = document.createElement("button")
 		button.textContent = "Add to Bag";
 		button.classList.add("addtobag")
 		button.dataset.id = elem.id;
 		button.addEventListener("click", addtocart);
-	
+
 		cardDiv.append(image_a, title_price, desp, button);
 		content.appendChild(cardDiv)
-		
+
 	})
 }
 
 
-function addtocart(event){
+function addtocart(event) {
 	const cart_data = JSON.parse(localStorage.getItem("cart_data")) || [];
 	const button = event.target;
 	let id = button.dataset.id;
@@ -158,14 +158,14 @@ function addtocart(event){
 	console.log(id);
 	let idFound = false;
 	cart_data.forEach((elem) => {
-		if(elem.id ===  id){
+		if (elem.id === id) {
 			elem.id = id;
-			elem.quantity = elem.quantity +1;
+			elem.quantity = elem.quantity + 1;
 			idFound = true
-			
+
 		}
 	})
-	if(!idFound){
+	if (!idFound) {
 		const obj = {
 			id: id,
 			quantity: 1,
@@ -176,22 +176,66 @@ function addtocart(event){
 }
 
 
-function debounce(func, timeout = 300){
+function debounce(func, timeout = 1000) {
 	let timer;
 	return (...args) => {
-	  clearTimeout(timer);
-	  timer = setTimeout(() => { func.apply(this, args); }, timeout);
+		clearTimeout(timer);
+		timer = setTimeout(() => { func.apply(this, args); }, timeout);
 	};
-  }
-	
-async function saveInput(){
+}
+
+async function saveInput() {
 	let searchData = document.querySelector("#search").value;
-	if(searchData < 1){
+	if (searchData < 1) {
 		renderPage();
-	}else{
-		console.log("sheeesh");
+	} else {
+		const content = document.querySelector(".content");
+		let response = await fetch(`http://localhost:3000/data?q=${searchData}`);
+		let data = await response.json();
+		console.log(data);
+		if(data.length === 0){
+			content.innerHTML = "Nothing Found"
+		}
+		else{
+
+			content.innerHTML = "";
+			data.forEach((elem) => {
+				const cardDiv = document.createElement("div");
+				cardDiv.classList.add("card");
+	
+				const image_a = document.createElement("a");
+				const primary_image = document.createElement("img");
+				primary_image.src = `${elem.primary_image}`;
+				image_a.href = `product_details.html/?id=${elem.id}`
+				image_a.appendChild(primary_image);
+	
+				const title_price = document.createElement("div");
+				title_price.classList.add("title-price");
+				const title = document.createElement("h3");
+				title.classList.add("title");
+				title.textContent = elem.title;
+				const price = document.createElement("p");
+				price.textContent = `$${elem.price}`
+				price.classList.add("price");
+				title_price.append(title, price);
+	
+				const desp = document.createElement("p");
+				desp.classList.add("desp");
+				desp.textContent = elem.desc;
+	
+				const button = document.createElement("button")
+				button.textContent = "Add to Bag";
+				button.classList.add("addtobag")
+				button.dataset.id = elem.id;
+				button.addEventListener("click", addtocart);
+	
+				cardDiv.append(image_a, title_price, desp, button);
+				content.appendChild(cardDiv)
+	
+			})
+		}
 	}
 }
-  
+
 const processChanges = debounce(() => saveInput());
 renderPage();
